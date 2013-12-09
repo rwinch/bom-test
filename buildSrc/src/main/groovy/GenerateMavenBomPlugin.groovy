4e77bc9bd03adde8cf9a23b12cdf9f5eee4575f5
Rob Winch <rwinch@vmware.com>
2013-12-09 13:46:10 -0600

    saving

--- buildSrc/src/main/groovy/GenerateMavenBomPlugin.groovy
+++ buildSrc/src/main/groovy/GenerateMavenBomPlugin.groovy
@@ -11,9 +11,8 @@ public class GenerateMavenBomPlugin implements Plugin<Project> {
 	static String MAVEN_BOM_TASK_NAME = "mavenBom"
 
 	public void apply(Project project) {
-		project.plugins.apply(MavenPlugin)
 		project.plugins.apply(JavaPlugin)
+		project.plugins.apply(MavenPlugin)
 		project.task(MAVEN_BOM_TASK_NAME, type: GenerateMavenBomTask, group: 'Generate', description: 'Generates a Maven Build of Materials (BOM)')
-		project.install.dependsOn project.mavenBom
 	}
 }
--- buildSrc/src/main/groovy/GenerateMavenBomTask.groovy
+++ buildSrc/src/main/groovy/GenerateMavenBomTask.groovy
@@ -8,33 +8,72 @@ public class GenerateMavenBomTask extends DefaultTask {
 
 	Set<Project> projects
 
+	String artifactId
+
+	String groupId
+
 	Closure<MarkupBuilder> customizePom
+
+	File pomFile
 
 	public GenerateMavenBomTask() {
 		this.group = "Generate"
 		this.description = "Generates a Maven Build of Materials (BOM)"
+		this.artifactId = "${->project.name}-bom"
 		this.projects = project.subprojects
+		this.pomFile = project.file("${->project.buildDir}/maven-bom/${->artifactId}-${->project.version}.pom")
 	}
 
 	@TaskAction
-	public void configureBom() {
+	public void generateMavenBom() {
 		project.configurations.archives.artifacts.clear()
-		
+		if(!groupId) {
+			this.groupId = project.group
+		}
+		pomFile.parentFile.mkdirs()
+		def xml = new MarkupBuilder(new OutputStreamWriter(new FileOutputStream(pomFile)))
+		xml.project(xmlns: 'http://maven.apache.org/POM/4.0.0',
+				'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+				'xsi:schemaLocation' : 'http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd') {
+			modelVersion('4.0.0')
+			delegate.groupId(groupId)
+			delegate.artifactId(artifactId)
+			delegate.version(project.version)
+			packaging('pom')
+			name(artifactId)
+			description("Bill of Materials (BOM)")
+			if(customizePom != null) {
+				customizePom.call(xml)
+			}
+			dependencyManagement {
+				projects.sort { dep -> "$dep.group:$dep.name" }.each { p ->
+					dependency {
+						delegate.groupId(p.group)
+						delegate.artifactId(p.name)
+						delegate.version(p.version)
+					}
+				}
+			}
+		}
+		project.artifacts {
+			// work around GRADLE-2406 by attaching text artifact
+			archives(pomFile)
+		}
 		project.install {
-			repositories.mavenInstaller {
-				pom.whenConfigured {
-					packaging = "pom"
-					withXml {
-						asNode().children().last() + {
-							delegate.dependencyManagement {
-								delegate.dependencies {
-									projects.sort { "$it.name" }.each { p ->
+		repositories.mavenInstaller {
+			pom.whenConfigured {
+				packaging = "pom"
+				withXml {
+					asNode().children().last() + {
+						delegate.dependencyManagement {
+							delegate.dependencies {
+								projects.sort { "$it.name" }.each { p ->
+									
 										delegate.dependency {
 											delegate.groupId(p.group)
 											delegate.artifactId(p.name)
 											delegate.version(p.version)
 										}
-									}
 								}
 							}
 						}
@@ -42,10 +81,6 @@ public class GenerateMavenBomTask extends DefaultTask {
 				}
 			}
 		}
-		
-		project.artifacts {
-			// work around GRADLE-2406 by attaching text artifact
-			archives(project.file("readme.txt"))
-		}
+	}
 	}
 }
