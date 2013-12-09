9a70410071ee2e6ee7518d44cdf3cc11000a8d74
Rob Winch <rwinch@vmware.com>
2013-12-09 12:01:38 -0600

    Fix it?

--- buildSrc/src/main/groovy/GenerateMavenBomPlugin.groovy
+++ buildSrc/src/main/groovy/GenerateMavenBomPlugin.groovy
@@ -3,7 +3,7 @@ import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.plugins.*
 import org.gradle.api.tasks.*
 import org.gradle.api.tasks.TaskAction
 
@@ -11,7 +11,9 @@ public class GenerateMavenBomPlugin implements Plugin<Project> {
 	static String MAVEN_BOM_TASK_NAME = "mavenBom"
 
 	public void apply(Project project) {
-		project.plugins.apply(BasePlugin)
+		project.plugins.apply(MavenPlugin)
+		project.plugins.apply(JavaPlugin)
 		project.task(MAVEN_BOM_TASK_NAME, type: GenerateMavenBomTask, group: 'Generate', description: 'Generates a Maven Build of Materials (BOM)')
+		project.install.dependsOn project.mavenBom
 	}
 }
